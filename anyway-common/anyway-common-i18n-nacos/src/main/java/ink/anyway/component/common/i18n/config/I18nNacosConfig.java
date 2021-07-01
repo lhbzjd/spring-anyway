72d02a9b2501ff3bbcc826ba081c3130cf1d11dd
lhbzjd@163.com <lhbzjd@163.com>
2021-07-01 19:12:15 +0800

    initial

---
+++ anyway-common/anyway-common-i18n-nacos/src/main/java/ink/anyway/component/common/i18n/config/I18nNacosConfig.java
@@ -0,0 +1,147 @@
+package ink.anyway.component.common.i18n.config;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.PropertyKeyConst;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import ink.anyway.component.common.properties.I18nMessagesProperties;
+import ink.anyway.component.common.util.StringUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
+@Slf4j
+@Component
+@ConditionalOnProperty(prefix = "spring.anyway.messages", name = "basename")
+public class I18nNacosConfig {
+
+    private String serverAddr;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Autowired
+    private I18nMessagesProperties i18nMessagesProperties;
+
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    private String i18nNamespace = "";
+    private String i18nGroup = "i18n";
+
+    @PostConstruct
+    public void init() {
+        serverAddr = applicationContext.getEnvironment().getProperty("spring.cloud.nacos.server-addr");
+        i18nNamespace = this.i18nMessagesProperties.getNacosNamespace();
+        if (StringUtil.isValid(i18nNamespace)) {
+            i18nNamespace = i18nNamespace.trim();
+        }else{
+            i18nNamespace = "";
+        }
+
+        i18nGroup = this.i18nMessagesProperties.getNacosGroup();
+        if (StringUtil.isValid(i18nGroup)) {
+            i18nGroup = i18nGroup.trim();
+        }else {
+            i18nGroup = "i18n";
+        }
+
+        initTip(null);
+        if(i18nMessagesProperties.getLocaleCollection()!=null&&i18nMessagesProperties.getLocaleCollection().size()>0){
+            for(String locale : i18nMessagesProperties.getLocaleCollection()){
+                if(StringUtil.isValid(locale)){
+                    String[] localeArr = locale.trim().split("_");
+                    if(localeArr!=null){
+                        List<String> localeList = new ArrayList<>();
+                        for(String mo:localeArr){
+                            if(StringUtil.isValid(mo))
+                                localeList.add(mo.trim());
+                        }
+
+                        if(localeList.size()==1)
+                            initTip(new Locale(localeList.get(0)));
+                        if(localeList.size()==2)
+                            initTip(new Locale(localeList.get(0), localeList.get(1)));
+                    }
+                }
+            }
+        }
+
+        log.info("init i18n parameters success! application name:{}, nacos addr:{}, i18n messages namespace:{}", applicationName, serverAddr, i18nNamespace);
+    }
+
+    private void initTip(Locale locale) {
+        String dataId = null;
+        try {
+            String content = null;
+
+            if (locale == null) {
+                dataId = i18nMessagesProperties.getBasename();
+            } else {
+                dataId = i18nMessagesProperties.getBasename() + "_" + locale.getLanguage();
+                if(StringUtil.isValid(locale.getCountry())){
+                    dataId+=("_" + locale.getCountry());
+                }
+            }
+            Properties properties = new Properties();
+            properties.put(PropertyKeyConst.SERVER_ADDR, serverAddr);
+            if(StringUtil.isValid(i18nNamespace)){
+                properties.put(PropertyKeyConst.NAMESPACE, i18nNamespace);
+            }
+            ConfigService configService = NacosFactory.createConfigService(properties);
+            content = configService.getConfig(dataId, i18nGroup, 5000);
+            if (!StringUtil.isValid(content)) {
+                log.warn("i18n parameter is null, skip this! dataId:{}", dataId);
+                return;
+            }
+            log.info("i18n parameter[{}] will init!", dataId);
+            saveAsFileWriter(dataId, content);
+            setListener(configService, dataId, locale);
+        } catch (Exception e) {
+            log.error(StringUtil.compose("i18n parameter [", dataId, "] have cached exception!"), e);
+        }
+    }
+
+    private void setListener(ConfigService configService, String dataId, Locale locale) throws com.alibaba.nacos.api.exception.NacosException {
+        configService.addListener(dataId, i18nGroup, new Listener() {
+            @Override
+            public void receiveConfigInfo(String configInfo) {
+                log.info("receive new i18n parameters! refreshing...");
+                try {
+                    initTip(locale);
+                } catch (Exception e) {
+                    log.error("refresh i18n parameters exception!", e);
+                }
+            }
+
+            @Override
+            public Executor getExecutor() {
+                return null;
+            }
+        });
+    }
+
+    private void saveAsFileWriter(String dataId, String content) {
+        try {
+            String fileName = StringUtil.compose(System.getProperty("user.dir"), File.separator, i18nMessagesProperties.getBaseFolder(), File.separator, dataId, ".properties");
+            File file = new File(fileName);
+            FileUtils.writeStringToFile(file, content);
+            log.info("i18n parameters have cached! local cache file:{}", fileName);
+        } catch (Exception e) {
+            log.error("i18n parameter ["+dataId+"] have cached exception!", e);
+        }
+    }
+
+}
---
+++ anyway-common/anyway-common-util/src/main/java/ink/anyway/component/common/properties/I18nMessagesProperties.java
@@ -0,0 +1,45 @@
+package ink.anyway.component.common.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RefreshScope
+@Data
+@Service
+@ConfigurationProperties(prefix = "spring.anyway.messages")
+public class I18nMessagesProperties {
+
+    /**
+     * 国际化文件缓存目录
+     */
+    private String baseFolder;
+
+    /**
+     * 国际化文件基础名称
+     */
+    private String basename;
+
+    /**
+     * 国际化编码格式
+     */
+    private String encoding;
+
+    /**
+     * 缓存刷新的时间间隔
+     */
+    private long cacheMillis;
+
+    private String nacosNamespace;
+
+    private String nacosGroup;
+
+    /**
+     * 提供支持的语言集合
+     */
+    private List<String> localeCollection;
+
+}
