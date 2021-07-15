71eec7683c0f88e44ccecc31dde8b4b399bd3d9f
lhbzjd@163.com <lhbzjd@163.com>
2021-07-15 18:02:50 +0800

    initial

--- anyway-common/anyway-common-util/src/main/java/ink/anyway/component/common/s3/top/OssClient.java
+++ anyway-common/anyway-common-util/src/main/java/ink/anyway/component/common/s3/top/OssClient.java
@@ -1,7 +1,9 @@
 package ink.anyway.component.common.s3.top;
 
 import com.amazonaws.AmazonClientException;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
 
 import java.io.InputStream;
@@ -12,8 +14,9 @@ public interface OssClient {
 
     public PutObjectResult putObject(String bucketName, String objectKey, InputStream input, ObjectMetadata metadata) throws AmazonClientException;
 
+    public PutObjectResult putObject(PutObjectRequest putObjectRequest) throws SdkClientException;
+
     public void deleteObject(String bucketName, String objectKey) throws AmazonClientException;
 
-    public void setBucketPolicy(String bucketName, String policy);
 
 }
